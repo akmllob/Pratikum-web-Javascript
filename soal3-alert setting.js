3b6b75e353ea1543bc81d744edebf0a7cebfbefa
Akmal <112837181+akmllob@users.noreply.github.com>
2023-10-11 19:02:13 +0700

    Create soal3-alert setting.js

---
+++ soal3-alert setting.js
@@ -0,0 +1,10 @@
+Ubahalertahay = () => {
+    var element = document.getElementById('pesan');
+    var dropdown = document.getElementById('alertTypeDropdown');
+    element.className = "";
+    element.classList.add("alert");
+    element.classList.add("alert-" + dropdown.value);
+    
+    var customMessage = document.getElementById('customMessage').value;
+    element.innerHTML = ` ${customMessage}`;
+}
